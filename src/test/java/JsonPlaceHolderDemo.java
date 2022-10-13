13f27d5b0768c86b94ada4d65eabffd6106cc57f
JavaClass <aleksroslik@gmail.com>
2022-10-13 11:46:59 +0200

    queryParams

--- src/test/java/JsonPlaceHolderDemo.java
+++ src/test/java/JsonPlaceHolderDemo.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class JsonPlaceHolder extends BaseTest {
+public class JsonPlaceHolderDemo extends BaseTest {
 
     private final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     private final String USERS = "users";
@@ -43,5 +43,31 @@ public class JsonPlaceHolder extends BaseTest {
         JsonPath jsonPath = response.jsonPath();
         String username = jsonPath.get("username");
         assertThat(username).isEqualTo("Bret");
+    }
+
+    @Test
+    public void getUserWithPathParam() {
+        given()
+                .pathParam("userId", "1").
+        when()
+                .get(BASE_URL + USERS + "/{userId}").
+        then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void getUserWithName() {
+        Response response =
+                given().queryParam("website", "hildegard.org")
+                       .queryParam("address.city", "Gwenborough").
+                when()
+                        .get(BASE_URL + USERS + "/1").
+                then()
+                        .statusCode(200)
+                        .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        String username = jsonPath.get("website");
+        assertThat(username).isEqualTo("hildegard.org");
     }
 }
